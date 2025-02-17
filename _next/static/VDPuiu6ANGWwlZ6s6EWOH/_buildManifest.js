24eece9930478142bb856592e2449042d074099b
EliasHdzR <2230200@upv.edu.mx>
2025-02-16 19:49:15 -0600

    Updates

---
+++ _next/static/VDPuiu6ANGWwlZ6s6EWOH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f7bac67f9d9badbc.css","static/chunks/pages/index-56761559004263d5.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
